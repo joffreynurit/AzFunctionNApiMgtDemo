6fd546f025dbce88d6049e10c67a4fa109a8b7d7
Joffrey NURIT <4489033+joffreynurit@users.noreply.github.com>
2022-06-21 14:19:26 +0200

    Getting started

---
+++ RngGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzFuncNApiMgtDemo
+{
+    public static class RngGenerator
+    {
+        [FunctionName("RngGenerator")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                Random random = new Random();
+
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+                int maxValue = data.MaxValue;
+
+                return new OkObjectResult(random.Next(maxValue+1));
+            }
+            catch (Exception ex) { }
+
+            return new BadRequestObjectResult(string.Empty);
+        }
+    }
+}
